116069cf628d18327159294f6d0a1f083c2c518f
chibinaoko <tranghuyenttht@gmail.com>
2016-08-22 13:42:49 +0700

    api-manage-order

---
+++ app/controllers/api/v1/orders_controller.rb
@@ -0,0 +1,59 @@
+class Api::V1::OrdersController < ApplicationController
+  skip_before_action :verify_authenticity_token, except: [:index, :new, :show]
+  before_action :authenticate_with_token!
+  before_action :find_order, only: [:update, :destroy, :show]
+
+  def index
+    @orders =  Order.all
+    render json: @orders
+  end
+
+  def show
+    render json: @order
+  end
+
+  def create
+    @order = current_user.orders.new order_params
+    if @order.save
+      render json: @order, serializer: OrderSerializer,
+        messages: t("api.order.create_success")
+    else
+      render json: {messages: t("api.order.create_fail")}
+    end
+  end
+
+  def update
+    unless @order.present?
+      render json: {messages: t("api.order.not_found")}
+    else
+      if @order.update_attributes order_params
+        render json: @order, serializer: OrderSerializer,
+          messages: t("api.order.update_success")
+      else
+        render json: {messages: t("api.order.update_success")}
+      end
+    end
+  end
+
+  def destroy
+    unless @order.present?
+      render json: {messages: t("api.order.not_found")}
+    else
+      if @order.destroy
+        render json: {messages: t("api.order.delete_success")}
+      else
+        render json: {messages: t("api.order.delete_fail")}
+      end
+    end
+  end
+
+  private
+  def order_params
+    params.require(:order).permit :user_id, :created_date,
+      :total_price, order_items_attributes: [:id, :item_id, :quantity]
+  end
+
+  def find_order
+    @order = Order.find_by id: params[:id]
+  end
+end
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,6 +1,18 @@
 class Order < ActiveRecord::Base
-  has_many :order_items
+  has_many :order_items, dependent: :destroy
   has_many :items, through: :order_items
   belongs_to :manager
   belongs_to :user
+
+  accepts_nested_attributes_for :order_items, allow_destroy: true
+  validates :created_date, presence: true
+
+  before_save :calculate_price
+
+  private
+  def calculate_price
+    self.order_items.each do |order_item|
+      self.total_price += order_item.item.price * order_item.quantity
+    end
+  end
 end
